c81f90e77d52d1880700e809f5490b2c39ed31d0
João Otávio <jofs2006@gmail.com>
2024-03-08 14:34:47 -0300

    added palindrome code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PalindromoComponent } from './palindromo/palindromo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { PalindromoComponent } from './palindromo/palindromo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/palindromo/palindromo.component.ts
+++ src/app/palindromo/palindromo.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './palindromo.component.css'
 })
 export class PalindromoComponent {
+  //input para inserir o texto
+  inputText: string = '';
+  //verifica se é falso
+  isPalindromo: boolean = false;
 
+  checkPalindromo() {
+    //tira os caracteres especiais 
+    const word = this.inputText.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
+    //junta as palavras ou números que estão na ordem inversa com base
+    //na divisão da string
+    const reversedWord = word.split('').reverse().join('');
+    //verifica o tipo de dados e compara os dois valores
+    this.isPalindromo = (word === reversedWord);
+  }
 }
